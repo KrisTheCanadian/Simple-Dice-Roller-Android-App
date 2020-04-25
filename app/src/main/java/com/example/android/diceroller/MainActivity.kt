e68ac3e25f162ea415426491fe8d3c349df90d07
KrisTheCanadian <kristhecanadian101@gmail.com>
2020-04-25 15:18:43 -0400

    coding challenge Cout up

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -12,13 +12,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        val countButton: Button = findViewById(R.id.count_up)
         rollButton.setOnClickListener { rollDice() }
+        countButton.setOnClickListener { countUp() }
     }
     private fun rollDice(){
-        //Toast.makeText(this, "button clicked",
-        //Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
     }
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        //edge cases
+        if (resultText.text == "Hello World!"){
+            resultText.text = "1"
+        }
+        else {
+            var resultInt = Integer.parseInt(resultText.text.toString())
+
+            if(resultInt < 6){
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
+    }
 }
+
